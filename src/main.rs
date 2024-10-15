46fba328f5c6585f322ebbbc9fe4c73c4708fcde
DMGDy <dylangarza1909@gmail.com>
2024-10-15 12:23:09 -0400

    main: adding header to response of file

--- src/main.rs
+++ src/main.rs
@@ -94,8 +94,21 @@ async fn main() {
     let data_store_filter = warp::any()
         .map(move || data_store.clone());
 
+    /*
     let get_bst_data = warp::get()
-        .and(warp::fs::file("/data/BST-test.csv"));
+        .and(warp::fs::file("/data/BST-test.csv"))
+        .with(warp::reply::he)
+    */
+
+    let get_bst_data = warp::get()
+        .and(warp::fs::file("data/BST-test.csv"))
+        .map(|file| {
+            warp::reply::with_header(
+                file,
+                "Content-Type",
+                "text/csv"
+            )
+        });
         
 
     let is_up_route = warp::get()
